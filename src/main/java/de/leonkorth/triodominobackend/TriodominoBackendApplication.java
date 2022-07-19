3eb7e54ffcc1a390a40e460ac201705e08f495ce
leonkorth <leonkorth@web.de>
2022-07-19 13:44:41 +0200

    initial

---
+++ src/main/java/de/leonkorth/triodominobackend/TriodominoBackendApplication.java
@@ -0,0 +1,13 @@
+package de.leonkorth.triodominobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriodominoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriodominoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/leonkorth/triodominobackend/TriodominoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.leonkorth.triodominobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriodominoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
