110f9c4187ccdaed243b15eb77aa26e45f4b781f
leonkorth <leonkorth@web.de>
2022-07-30 12:27:04 +0200

    game controller

---
+++ src/main/java/de/leonkorth/triodominobackend/service/GameService.java
@@ -0,0 +1,21 @@
+package de.leonkorth.triodominobackend.service;
+
+import de.leonkorth.triodominobackend.persistence.entities.Game;
+import de.leonkorth.triodominobackend.persistence.entities.Player;
+import de.leonkorth.triodominobackend.persistence.repos.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    @Autowired
+    GameRepository repository;
+
+    public List<Game> findAll() {return repository.findAll();}
+
+    public Game createGame(Game game) {return repository.save(game);}
+
+}
---
+++ src/main/java/de/leonkorth/triodominobackend/web/GameController.java
@@ -0,0 +1,26 @@
+package de.leonkorth.triodominobackend.web;
+
+import de.leonkorth.triodominobackend.persistence.entities.Game;
+import de.leonkorth.triodominobackend.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+public class GameController {
+
+    @Autowired
+    GameService service;
+
+    @PostMapping(path = "/api/v1/games")
+    public Game createGame(@Valid @RequestBody Game game){return service.createGame(game);}
+
+    @GetMapping(path = "/api/v1/games")
+    public ResponseEntity<List<Game>> getGames() {return ResponseEntity.ok(service.findAll());}
+}
