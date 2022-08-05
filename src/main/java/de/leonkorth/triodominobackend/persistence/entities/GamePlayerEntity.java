a8672ce47d7355777165574e6d2dee1b9d8d63c8
leonkorth <leonkorth@web.de>
2022-08-05 09:29:30 +0200

    finished gamePlayerEntity

--- src/main/java/de/leonkorth/triodominobackend/persistence/entities/GamePlayerEntity.java
+++ src/main/java/de/leonkorth/triodominobackend/persistence/entities/GamePlayerEntity.java
@@ -5,6 +5,9 @@ import de.leonkorth.triodominobackend.service.PlayerService;
 import org.hibernate.annotations.ManyToAny;
 
 import javax.persistence.*;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.Objects;
 
 @Entity(name = "gamePlayer")
 public class GamePlayerEntity {
@@ -23,9 +26,88 @@ public class GamePlayerEntity {
     private PlayerEntity playerEntity;
 
 
+    @Column(name = "placement")
+    @Positive(message = "value must be positive")
     private int placement;
+
+
+    @Column(name = "points")
     private int points;
+
+   @Column(name = "draws")
+   @PositiveOrZero
     private int draws;
 
+    public GamePlayerEntityPK getId() {
+        return id;
+    }
 
+    public void setId(GamePlayerEntityPK id) {
+        this.id = id;
+    }
+
+    public GameEntity getGameEntity() {
+        return gameEntity;
+    }
+
+    public void setGameEntity(GameEntity gameEntity) {
+        this.gameEntity = gameEntity;
+    }
+
+    public PlayerEntity getPlayerEntity() {
+        return playerEntity;
+    }
+
+    public void setPlayerEntity(PlayerEntity playerEntity) {
+        this.playerEntity = playerEntity;
+    }
+
+    public int getPlacement() {
+        return placement;
+    }
+
+    public void setPlacement(int placement) {
+        this.placement = placement;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public int getDraws() {
+        return draws;
+    }
+
+    public void setDraws(int draws) {
+        this.draws = draws;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GamePlayerEntity that = (GamePlayerEntity) o;
+        return placement == that.placement && points == that.points && draws == that.draws && Objects.equals(id, that.id) && Objects.equals(gameEntity, that.gameEntity) && Objects.equals(playerEntity, that.playerEntity);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, gameEntity, playerEntity, placement, points, draws);
+    }
+
+    public GamePlayerEntity() {
+    }
+
+    public GamePlayerEntity(GamePlayerEntityPK id, GameEntity gameEntity, PlayerEntity playerEntity, int placement, int points, int draws) {
+        this.id = id;
+        this.gameEntity = gameEntity;
+        this.playerEntity = playerEntity;
+        this.placement = placement;
+        this.points = points;
+        this.draws = draws;
+    }
 }
